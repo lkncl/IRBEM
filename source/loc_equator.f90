a79fef896407523c39d78fa6d7c001728c8e4682
Lucas Nicolle <lucas.nicolle@onera.fr>
2025-12-02 18:42:26 +0100

    add flexible arrays entry point for loc_equator and manage retrocompatiblity

--- source/loc_equator.f90
+++ source/loc_equator.f90
@@ -20,55 +20,60 @@
        SUBROUTINE loc_equator(lati,longi,alti,Bmin,posit)
 !
        IMPLICIT NONE
-       REAL*8     lati,longi,alti
-       REAL*8     Bmin
-       REAL*8     posit(3)
-       REAL*8     xx0(3)
-       
+
+       INTEGER*4  Nreb
+       PARAMETER (Nreb = 50)
+
+       REAL(8), INTENT(IN) :: lati, longi, alti
+       REAL(8), INTENT(OUT) :: Bmin, posit(3)
+
+       REAL(8) :: xx0(3)
+
        CALL GDZ_GEO(lati,longi,alti,xx0(1),xx0(2),xx0(3))
-       
-       call loc_equator_opt(xx0,Bmin,posit)
+       call loc_equator_flex(xx0,Bmin,posit, Nreb)
 
        RETURN
        END
 
        SUBROUTINE loc_equator_opt(xx0,Bmin,posit)
-!
+
+       IMPLICIT NONE
+
+       INTEGER*4  Nreb
+       PARAMETER (Nreb = 50)
+
+       REAL(8), INTENT(IN) :: xx0(3)
+       REAL(8), INTENT(OUT) :: Bmin, posit(3)
+
+       call loc_equator_flex(xx0,Bmin,posit, Nreb)
+       END SUBROUTINE
+
+       SUBROUTINE loc_equator_flex(xx0,Bmin,posit, Nreb)
+
        USE fieldline_utils
        IMPLICIT NONE
        INCLUDE 'variables.inc'
-!
-       INTEGER*4  Nreb
-       PARAMETER (Nreb = 50)
-!
-       INTEGER*4  Nrebmax
-       REAL*8     xx0(3)
-       REAL*8     B(3),B0,Bmin
-       REAL*8     dsreb
 
-       INTEGER*4  Ifail
-       REAL*8     Lb
-!
-!
-       REAL*8     posit(3)
-!
-!
+       REAL(8), INTENT(IN) :: xx0(3)
+       INTEGER(4), INTENT(IN) :: Nreb
+       REAL(8), INTENT(OUT) :: Bmin, posit(3)
+
+       INTEGER(4) :: Nrebmax, Ifail
+       REAL(8) :: B(3),B0
+       REAL(8) :: dsreb, Lb
+
        Nrebmax = 10*Nreb
-!
+       posit(1) = baddata
+       posit(2) = baddata
+       posit(3) = baddata
+       Bmin=baddata
+
        CALL COMPUTE_L_DIPOLE(xx0, Lb)
-!
        CALL CHAMP(xx0,B,B0,Ifail)
-       IF (Ifail.LT.0) THEN
-              Bmin=baddata
-              posit(1) = baddata
-              posit(2) = baddata
-              posit(3) = baddata
-              RETURN
-       ENDIF
-       Bmin = B0
+       IF (Ifail.LT.0) RETURN
 
        dsreb = Lb/Nreb
        CALL FIND_MAGEQUATOR(xx0, B0, dsreb, nrebmax, posit, Bmin)
 
-       END
+       END SUBROUTINE
        
